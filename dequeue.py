5d88fa9103425cc3ddf524cd8056b1f8bdcb6df9
liciascl <licia.sales@hotmail.com>
2022-06-22 15:12:39 -0300

    dequeue

---
+++ dequeue.py
@@ -0,0 +1,60 @@
+
+import requests
+import json
+
+import os
+import lightkurve as lk
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.io import fits
+
+
+site="http://192.168.50.10:8080/memq/server/queues/lista/dequeue"
+resposta = requests.post(site)
+
+missao = 'TESS'
+setor = '2'
+
+while resposta.status_code is not 204:
+
+    dados = json.loads(resposta.text)
+    target_id = dados['body']
+    print(missao,setor,target_id)
+    search_result = lk.search_lightcurve(target_id, mission=missao, sector=setor)
+    lc = search_result.download(quality_bitmask='default')
+    resposta = requests.post(site)
+    try:
+        lc = lc.remove_nans().remove_outliers()
+        lc.scatter()
+        filename = "output/%s.fits" % target_id
+        lc.to_fits(path=filename, overwrite=True)    
+        # Create array of periods to search
+        period = np.linspace(1,20, 10000)
+        # Create a BLSPeriodogram
+        bls = lc.to_periodogram(method='bls', period=period, frequency_factor=500)
+
+        # Searching for the biggest transit
+        planet_x_period = bls.period_at_max_power
+        planet_x_t0 = bls.transit_time_at_max_power
+        planet_x_dur = bls.duration_at_max_power
+
+        # Check the value for period
+        print("Check the value for period",planet_x_period)
+
+        # Create a BLS model using the BLS parameters
+        planet_x_model = bls.get_transit_model(period=planet_x_period,
+                                            transit_time=planet_x_t0,
+                                            duration=planet_x_dur)
+
+        ax = lc.fold(planet_x_period, planet_x_t0).scatter()
+        planet_x_model.fold(planet_x_period, planet_x_t0).plot(ax=ax, c='r', lw=2)
+        ax.set_xlim(-5, 5)
+        plt.savefig('output/period_found_{:.2f}.png'.format(planet_x_period))
+    
+        
+        print("=================================NEXT TARGET ===============================")
+
+    except:
+        print("PROBLEM WITH TARGET",target_id)
+
+
--- web_scraping.py
+++ web_scraping.py
@@ -4,21 +4,27 @@ from bs4 import BeautifulSoup
 
 regex = r"^\s*(\d*)\s"
 lista=[]
-number = 1
-for number in range (1,54): 
-    target = "all_targets_S{:03}".format(number)
-    url = f"https://tess.mit.edu/wp-content/uploads/{target}_v1.txt"
-    print (url)
-    res = requests.get(url)
-    nums = [x.split("\t")[0] for x in res.text.split("\n")]
-    
-    for n in nums:        
-        try:
-            toi=(int(n.strip()))            
-            lista.insert(number,toi)
-            print ("In the Sector ", number)
-            print(" We get the TOI ",toi)
-        except:
-            pass 
+number = 2
 
+site="http://192.168.50.10:8080/memq/server/queues/lista/enqueue/"
+
+
+
+#for number in range (1,2): 
+#target = "all_targets_S{:03}".format(number)
+target = "all_targets_S{:03}".format(number)
+url = f"https://tess.mit.edu/wp-content/uploads/{target}_v1.txt"
+print (url)
+res = requests.get(url)
+nums = [x.split("\t")[0] for x in res.text.split("\n")]
+        
+for n in nums:                    
+    try:
+        toi=(int(n.strip()))            
+        lista.insert(number,toi)
+        resposta = requests.post(site,str(toi))
+        print(resposta.text)
+
+    except:
+        print("Bad Format")
 
